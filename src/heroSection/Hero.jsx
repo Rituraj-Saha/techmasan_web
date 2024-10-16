045a0622e93727153b7672f1c0a54eecec8097a1
Rituraj Saha <rituraj10saha@gmail.com>
2024-10-17 01:38:25 +0530

    basic hero

--- src/heroSection/Hero.jsx
+++ src/heroSection/Hero.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import styles from "./Hero.module.css";
 import { heroSection } from "../constant/examplePayload";
+import { TypeAnimation } from "react-type-animation";
 const Hero = () => {
   return (
     <div
@@ -28,6 +29,22 @@ const Hero = () => {
         <span style={{ border: "1px solid black" }}>
           {heroSection.heroSectionMsg.title}
         </span>
+        <TypeAnimation
+          sequence={[
+            // Same substring at the start will only be typed once, initially
+            "We produce food for Mice",
+            1000,
+            "We produce food for Hamsters",
+            1000,
+            "We produce food for Guinea Pigs",
+            1000,
+            "We produce food for Chinchillas",
+            1000,
+          ]}
+          speed={50}
+          style={{ fontSize: "2em" }}
+          repeat={Infinity}
+        />
         <span style={{ border: "1px solid black" }}>
           {heroSection.heroSectionMsg.subTitle}
         </span>
