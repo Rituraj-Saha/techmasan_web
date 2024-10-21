d53752d55ef1acf2345ef72dd1b4010cb8067090
Rituraj Saha <rituraj10saha@gmail.com>
2024-10-21 20:52:10 +0530

    Upto Services

--- src/App.js
+++ src/App.js
@@ -4,10 +4,12 @@ import Hero from "./heroSection/Hero";
 import Services from "./services/Services";
 import Portfolio from "./portfolio/Portfolio";
 import Steps from "./steps/Steps";
+import NavigationBar from "./navigationBar/NavigationBar";
 
 function App() {
   return (
     <div className="App">
+      <NavigationBar />
       <Hero />
       <Services />
       <Portfolio />
